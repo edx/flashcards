2b436d2d95478fee271d1332030e22b48be3d744
Andy Shultz <ashultz@edx.org>
2023-10-25 13:15:25 -0400

    feat: endpoint

--- flashcards/apps/api/v1/urls.py
+++ flashcards/apps/api/v1/urls.py
@@ -1,4 +1,9 @@
 """ API v1 URLs. """
+from django.urls import path
+
+from . import views
 
 app_name = 'v1'
-urlpatterns = []
+urlpatterns = [
+    path('cards/<str:course_id>/<str:block_id>', views.cards, name='cards'),
+]
